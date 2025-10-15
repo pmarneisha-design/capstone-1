789e68f111adf4e2bfafd8fa44e8dcb07e8f5152
moe pordos <pmarneisha@gmail.com>
2025-10-15 18:54:22 -0500

    created filewriter and added makepayment

--- src/main/java/com/pluralsight/Capstone_1.java
+++ src/main/java/com/pluralsight/Capstone_1.java
@@ -1,16 +1,24 @@
 package com.pluralsight;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Capstone_1 {
+    static Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        mainMenu(scanner);
-        scanner.close();
+        mainMenu();
     }
     // scanner for user input
 
 
-    public static void mainMenu(Scanner scanner) {
+    public static void mainMenu() {
         String userInput;
 
         do {
@@ -19,52 +27,130 @@ public class Capstone_1 {
             System.out.println("P- Payment");
             System.out.println("L- Ledger");
             System.out.println("X- Exit");
-
-            userInput = getInput(scanner, "Please enter character for action");
+            System.out.println("Please enter character for action");
+            userInput = scanner.nextLine();
 
             switch (userInput) {
-                case "D" -> addDeposit();
-                case "P" -> makePayment();
-                case "L" -> ledgerMenu(scanner);
-                case "X" -> exitApplication();
-                default -> System.out.println("Invalid input. Try again.");
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    makePayment();
+               case "L":
+                   ledgerMenu();
+                case "X":
+                    break;
+//                default -> System.out.println("Invalid input. Try again.");
             }
         } while (userInput.equals("X"));
     }
 
-    // ADD DEPOSIT
+    // converts string into an amount and prompts user for deposit
     private static void addDeposit() {
-        System.out.println("Enter deposit amount");
+        // prompt user for date
+        System.out.println("Enter transaction date: YYYY-MM-DD");
+        String dateString = scanner.nextLine();
+        LocalDate date = LocalDate.parse(dateString);
+// prompt user for time
+        System.out.println("Enter transaction time: HH:MM ");
+        String timeString = scanner.nextLine();
+        LocalTime time = LocalTime.parse(timeString);
+// prompt user for description
+        System.out.println("Enter transaction description");
+        String descriptionString = scanner.nextLine();
+// prompts user for vendor name
+        System.out.println("Enter vendor name");
+        String vendorString = scanner.nextLine();
+        // prompts user to enter transaction amount
+        System.out.println("Enter transaction amount");
+        String amountString = scanner.nextLine();
+        double amount = Double.parseDouble(amountString);
+
+
+//date|time|description|vendor|amount
+        String ledgerEntry = date + "\\|" + time + "\\|" + descriptionString + "\\|" + vendorString + "\\|" + amount;
+        String fileName = "src/main/resources/transactions.csv";
+
+// fileWriter writes stream of characters to file
+    // open files
+   try (FileWriter fileWriter = new FileWriter(fileName,true)){
+       fileWriter.write(ledgerEntry + "\n");
+       System.out.println("Transaction completed!");
+
+} catch (IOException error){
+        System.out.println("Error saving transaction");
+//
+
+}
     }
 
+    // date, time, description, vendor, amount
     private static void makePayment() {
+        // prompt user for date
+        System.out.println("Enter transaction date: YYYY-MM-DD");
+        String dateString = scanner.nextLine();
+        LocalDate date = LocalDate.parse(dateString);
+// prompt user for time
+        System.out.println("Enter transaction time: HH:MM ");
+        String timeString = scanner.nextLine();
+        LocalTime time = LocalTime.parse(timeString);
+// prompt user for description
+        System.out.println("Enter transaction description");
+        String descriptionString = scanner.nextLine();
+// prompts user for vendor name
+        System.out.println("Enter vendor name");
+        String vendorString = scanner.nextLine();
+        // prompts user to enter transaction amount
+        System.out.println("Enter transaction amount");
+        String amountString = scanner.nextLine();
+        double amount = Double.parseDouble(amountString);
+        //negates amount because this is a payment (debit)
+        double debitAmount = -Math.abs(amount);
+
+        String ledgerEntry = date + "\\|" + time + "\\|" + descriptionString + "\\|" + vendorString + "\\|" + amount;
+
+        String fileName = "src/main/resources/transactions.csv";
+
+
+        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+            fileWriter.write(ledgerEntry + "\n");
+            System.out.println("Payment added!");
+
+        } catch (IOException error) {
+            System.out.println("Invalid payment");
+        }
     }
 
-    private static void ledgerMenu(Scanner scanner) {
-        do {
-            System.out.println("LEDGER MENU");
-            System.out.println("A- Display All Entries");
-            System.out.println("D- Deposits");
-            System.out.println("P- Payments");
-            System.out.println("R- Reporta");
-
-            userInput = getInput(scanner, "Please enter character for action");
-
-            switch (userInput) {
-                case "D" -> addDeposit();
-                case "P" -> makePayment();
-                case "L" -> ledgerMenu(scanner);
-                case "X" -> exitApplication();
-                default -> System.out.println("Invalid input. Try again.");
-            }
-        } while (userInput.equals("X"));
+    private static void ledgerMenu() {
+        ledgerMenu();
+        System.out.println("LEDGER MENU");
+        System.out.println("A- All Entries");
+        System.out.println();
     }
 
+
+//        } while (userInput.equals("X"));
+//    }
+
+
+
+//creating CLI financial transaction app to track all financial transactions
+// application must have several screens
+    //screens:
+    // display home screen
+    // home screen options: add deposit, make payment, ledger, exit
+    // if user clicks add deposit then they should be prompted to add transactions: date, time, description, vendor, amount
+    // create method- add deposit
+    //if the user clicks make a payment (use negative # because subtracting from account)
+    // prompt user for debit information
+    // method makePayment
+    //add formula to negate from account
+        // use file writer to write new payment on transaction file
+        //display the ledger screen: deposits, payments, reports
+    // ledger menu: a)all: display all transactions screen(newest first)
+    //
 
     private static void exitApplication() {
 
-    }
-
-    private static String getInput(Scanner scanner, String message) {
     }
 }
